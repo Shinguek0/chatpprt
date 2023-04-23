c94362ceb9f6feea2163a6af18b66f75bfd12ec5
ShinguekO <Shingueko_@hotmail.com>
2023-04-22 23:38:49 -0300

    new file:   pprt/.editorconfig 	new file:   pprt/.gitignore 	new file:   pprt/.vscode/extensions.json 	new file:   pprt/.vscode/launch.json 	new file:   pprt/.vscode/tasks.json     new file:   pprt/angular.json 	new file:   pprt/package-lock.json 	new file:   pprt/package.json 	new file:   pprt/src/app/app-routing.module.ts 	new file:   pprt/src/app/app.component.css 	new file:   pprt/src/app/app.component.html 	new file:   pprt/src/app/app.component.spec.ts 	new file:   pprt/src/app/app.component.ts 	new file:   pprt/src/app/app.module.ts 	new file:   pprt/src/app/chat/chat.component.css 	new file:   pprt/src/app/chat/chat.component.html 	new file:   pprt/src/app/chat/chat.component.spec.ts 	new file:   pprt/src/app/chat/chat.component.ts 	new file:   pprt/src/app/messages.service.spec.ts 	new file:   pprt/src/app/messages.service.ts 	new file:   pprt/src/assets/.gitkeep 	new file:   pprt/src/favicon.ico 	new file:   pprt/src/index.html 	new file:   pprt/src/main.ts 	new file:   pprt/src/styles.css 	new file:   pprt/tailwind.config.js 	new file:   pprt/tsconfig.app.json 	new file:   pprt/tsconfig.json 	new file:   pprt/tsconfig.spec.json

---
+++ pprt/src/app/chat/chat.component.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+import { MessagesService, Item, generateId } from "../messages.service";
+
+@Component({
+  selector: 'app-chat',
+  templateUrl: './chat.component.html',
+  styleUrls: ['./chat.component.css'],
+})
+
+export class ChatComponent {
+  contagem = 0;
+
+  nome = 'Lucas';
+  idade = 0;
+
+  alunos = [
+    { nome: 'Lucas', idade: 20 },
+    { nome: 'Bruna', idade: 19 },
+  ];
+  newMessageText: string = "";
+  errorMessage: string | null = null;
+
+  constructor(public MessagesService: MessagesService) {}
+
+  addMessage(){
+    const newMessage: Item = {
+      id: generateId(),
+      name: this.newMessageText,
+    };
+
+    if (newMessage.name !== "") {
+      this.MessagesService.addMessage(newMessage);
+      this.newMessageText = "";
+      this.errorMessage = null;
+    } else {
+      this.errorMessage = "ERRORRRRReeeeeeeeeeeeeeeeeeeeeeeeee";
+    }
+  }
+  incrementar() {
+    this.contagem++;
+  }
+  decrementar() {
+    this.contagem--;
+  }
+  adicionaAluno(aluno: { nome: string; idade: number }) {
+    this.alunos.push(aluno);
+  }
+}
---
+++ pprt/src/app/messages.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from "@angular/core";
+import { BehaviorSubject } from "rxjs";
+
+// typescript function that generate a random ID in string format
+export function generateId(): string {
+  const randomString: string = Math.random().toString(36).substring(2, 10);
+  return randomString + Date.now();
+}
+
+export interface Item {
+  id: string;
+  name: string;
+}
+
+@Injectable({
+  providedIn: "root",
+})
+export class MessagesService {
+  private itemsSource = new BehaviorSubject<Item[]>([]);
+  public items$ = this.itemsSource.asObservable();
+
+  constructor() {}
+
+  addMessage(item: Item) {
+    const items = this.itemsSource.getValue();
+    items.push(item);
+    this.itemsSource.next(items);
+  }
+}
